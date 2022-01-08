388073ce39402d34f606924a0e9446eb8aae7121
david-botelho-mariano <48680041+david-botelho-mariano@users.noreply.github.com>
2022-01-08 16:31:58 -0300

    v0.5

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import subprocess
 import os
+from xml.dom import minidom
 
 base_folder = os.getcwd() 
 app = Flask(__name__)
@@ -30,18 +31,65 @@ def amass():
     print(amass_result)
 
     try:
-        os.mkdir(base_folder + "/" + target)
+        os.mkdir(base_folder + "/targets/" + target)
 
     except Exception as error:
         pass
 
     finally:
-        amass_filename = base_folder + "/" + target + "/amass.txt"
+        amass_filename = base_folder + "/targets/" + target + "/amass.txt"
         print(amass_filename)
 
         save_file(amass_filename, amass_result)
     
     return str(amass_result)
+
+@app.route("/programs/nmap")    
+def nmap():
+
+    try:
+        os.mkdir(base_folder + "/targets/" + target)
+
+    except Exception as error:
+        pass
+
+    target = request.args.get("target")
+    subdomains_path = base_folder + "/targets/" + target + "/amass.txt"
+    output_path = base_folder + "/targets/" + target + "/nmap.xml"
+    nmap_filename_clean = base_folder + "/targets/" + target + "/nmap_clean.txt"
+
+    args = [base_folder + '/tools/nmap/nmap.exe', '-T5', '-F', '-Pn', '-iL', subdomains_path, '-oX', output_path]
+    print(args)
+
+    nmap_result = subprocess.check_output(args)
+    print(nmap_result)
+
+    nmap_result_clean = ""
+
+    xmldoc = minidom.parse(output_path)
+    hosts = xmldoc.getElementsByTagName("host")
+
+    for host in hosts:
+        hostnames = host.getElementsByTagName("hostnames")
+        
+        for hostname in hostnames:
+            hostname = hostname.getElementsByTagName("hostname")
+            hostname = hostname[0].attributes["name"].value
+            #print(hostname)
+
+            ports = host.getElementsByTagName("ports")
+
+            for port in ports:
+                port = host.getElementsByTagName("port")
+
+                for port in port:
+                    port = port.attributes["portid"]
+                    port = port.value
+                    nmap_result_clean += hostname + ":" + port + "\n"
+
+    save_file(nmap_filename_clean, nmap_result_clean)
+
+    return str(nmap_result)
 
 if __name__ == '__main__':    
 	app.run(debug=True)      
